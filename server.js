eb8989a65ba474c922d2f1d4aa59f43b6a52061c
sfreakinr <subinrajwwe@gmail.com>
2025-02-16 15:40:38 +0530

    updated server

--- server.js
+++ server.js
@@ -15,3 +15,21 @@ app.use('/api/products', require('./routes/productRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("✅ MongoDB Connected Successfully!");
+    } catch (error) {
+        console.error("❌ MongoDB Connection Error:", error);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+connectDB();
